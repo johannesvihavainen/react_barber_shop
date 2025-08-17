be7361838fb71df65edaae9ade309eb24c6e7a3d
Johannes Vihavainen <johannesvihavainen@outlook.com>
2025-08-17 13:04:52 +0300

    updated styling for mobiler responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,10 @@ export default function NavBar() {
         <nav>
             <h1>Texas Barber Shop</h1>
             <ul>
-                <li><Link className="nav-link">Home</Link></li>
-                <li><Link className="nav-link">Services</Link></li>
-                <li><Link className="nav-link">About Us</Link></li>
-                <li><Link className="nav-link">Gallery</Link></li>
+                <li><Link to="/" className="nav-link">Home</Link></li>
+                <li><Link to="/services" className="nav-link">Services</Link></li>
+                <li><Link to="/about-us" className="nav-link">About Us</Link></li>
+                <li><Link to="/gallery" className="nav-link">Gallery</Link></li>
             </ul>
         </nav>
     )
