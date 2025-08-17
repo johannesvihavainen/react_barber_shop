b490a655978217220e32b56295d6b626e87901df
Johannes Vihavainen <johannesvihavainen@outlook.com>
2025-08-17 20:13:47 +0300

    added functionality for booking to work with localhost

--- src/components/BookBarber.js
+++ src/components/BookBarber.js
@@ -1,3 +1,4 @@
+import Footer from './Footer'
 import NavBar from './Navbar'
 import { useEffect, useState } from 'react'
 
@@ -25,23 +26,28 @@ export default function BookBarber() {
 
     return (
         <div>
-            <div>
-                <h1>Book a Barber</h1>
+        <div className='booking'>
+            <h1 className='booking-title'>Book a Barber</h1>
+            <div className='booking-form'>
                 {barbers.map(barber => (
-                    <div key={barber.id}>
-                        <h2>{barber.name}</h2>
-                        <p>Services: {barber.services.join(', ')}</p>
-                        <h3>Available Slots:</h3>
-                        {barber.availableSlots.length > 0 ? (
-                            barber.availableSlots.map(slot => (
-                                <button key={slot} onClick={() => handleBook(barber.id, slot)}>{new Date(slot).toLocaleString()}</button>
-                            ))
-                        ) : (
-                            <p>No slots available</p>
-                        )}
+                    <div className='booking-container' key={barber.id}>
+                        <div className='book-card'>
+                            <h2>{barber.name}</h2>
+                            <p>Services: {barber.services.join(', ')}</p>
+                            <h3>Available Slots:</h3>
+                            {barber.availableSlots.length > 0 ? (
+                                barber.availableSlots.map(slot => (
+                                    <button className='booking-button' key={slot} onClick={() => handleBook(barber.id, slot)}>{new Date(slot).toLocaleString()}</button>
+                                ))
+                            ) : (
+                                <p>No slots available</p>
+                            )}
+                        </div>
                     </div>
                 ))}
             </div>
+        </div>
+        <Footer />
         </div>
     )
 }
