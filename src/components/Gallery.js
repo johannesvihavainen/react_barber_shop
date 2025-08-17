0700b4f7d0b494fd073aa4be60ab1741bb2eef53
Johannes Vihavainen <johannesvihavainen@outlook.com>
2025-08-17 14:20:40 +0300

    added Gallery.js and styling for it to be mobile responsive

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,48 @@
+import alex from '../assets/gallery/alex.jpg'
+import andre from '../assets/gallery/andre.jpg'
+import chris from '../assets/gallery/chris_knight.jpg'
+import megan from '../assets/gallery/megan.jpg'
+import michael1 from '../assets/gallery/michael_demoya.jpg'
+import michael2 from '../assets/gallery/michael_demoya2.jpg'
+import mostafa from '../assets/gallery/mostafa.jpg'
+import salah from '../assets/gallery/salah.jpg'
+import mitchell from '../assets/gallery/mitchell.jpg'
+import Footer from './Footer'
+
 export default function Gallery() {
     return (
-        <div>
-            
+        <div className='gallery'>
+            <h1 className='gallery-title'>Browse our gallery!</h1>
+            <div className="image-gallery">
+                <div className="image-card">
+                    <img src={alex} alt="alex's work" />
+                </div>
+                <div className="image-card">
+                    <img src={andre} alt="andre's work" />
+                </div>
+                <div className="image-card">
+                    <img src={chris} alt="chris's work" />
+                </div>
+                <div className="image-card">
+                    <img src={megan} alt="megan's work" />
+                </div>
+                <div className="image-card">
+                    <img src={michael1} alt="michael's work" />
+                </div>
+                <div className="image-card">
+                    <img src={michael2} alt="michael's work" />
+                </div>
+                <div className="image-card">
+                    <img src={mostafa} alt="mostafa's work" />
+                </div>
+                <div className="image-card">
+                    <img src={salah} alt="salah's work" />
+                </div>
+                <div className="image-card">
+                    <img src={mitchell} alt="mitchell's work" />
+                </div>
+            </div>
+            <Footer />
         </div>
     )
 }
